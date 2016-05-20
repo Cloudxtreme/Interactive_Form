c73425946ca02d01c26ad7f68610ca418b7c4875
Thuy Pham <tpham045@hotmail.com>
2016-05-20 16:36:40 -0700

    Add files and requirements in app.js

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Set focus on the first text field when the page load
+
+// Job Role: if "Other" option is selected, reveal a text field with id "other-title" and placeholder "Your Title"
+
+// T-Shirt Info: Display condition
+//1. If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+//2. If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+
+// Register for Activities
+//Grouping activities which are the same time together
+//If one activity of a group is selected, selected activity is active and checked, the rest of the group is disable
+//If none of activities in the same group are selected, all activities are active and uncheck
+
+// Display total cost
+
+// Display Payment Info based on chosen payment option
+
+// Form validation. Display error messages if:
+// 1.Name field is empty
+// 2.Email field must be a validly formatted e-mail address. Use a regular expression to get this requirement.
+// 3.At least one activity must be checked
+// 4.Payment option must be selected.
+// 5."Credit card" info is empty
+
+//X-credit: Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu.
+
+//X-credit: Style the "select" menus (drop down menus) on the form, so they match the styling of the text fields (see Resources links for an article on how to improve the look of select menus using CSS and JavaScript).
+
+//X-credit: Validate the credit card number so that it's a validly formatted credit card number. (see the Resources links for information on how to do this.)
